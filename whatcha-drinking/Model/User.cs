2c3da7c6880702bdc0b1a6b36f403032bc7b2704
r-stroud <stroudrobert86@gmail.com>
2023-05-18 18:28:08 -0500

    added User and NewUser model

---
+++ whatcha-drinking/Model/User.cs
@@ -0,0 +1,17 @@
+﻿namespace whatcha_drinking.Model
+{
+    public class User
+    {
+        public int FirebaseId { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string ProfilePic { get; set; }
+    }
+
+    public class NewUser
+    {
+        public int FirebaseId { get; set; }
+    }
+}
