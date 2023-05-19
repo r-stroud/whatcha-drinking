5b39c613b669e945b1a5d07b096f401fe636a7af
r-stroud <stroudrobert86@gmail.com>
2023-05-18 20:00:34 -0500

    added UserRepository, IUserRepository, BaseRepository, and Utility Class

---
+++ whatcha-drinking/Repositories/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using whatcha_drinking.Model;
+using whatcha_drinking.Utils;
+
+namespace whatcha_drinking.Repositories
+{
+    public class UserRepository : BaseRepository, IUserRepository
+    {
+        public UserRepository(IConfiguration configuration) : base(configuration) { }
+
+        public NewUser Add(NewUser user)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                        INSERT INTO [WhatchaDrinking].[dbo].[user](firebaseId)
+                                        OUTPUT inserted.firebaseId
+                                        VALUES (@firebaseId)";
+
+                    user.FirebaseId = (int)cmd.ExecuteScalar();
+
+                    return user;
+                }
+            }
+        }
+    }
+}
---
+++ whatcha-drinking/Utils/DbUtils.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace whatcha_drinking.Utils
+{
+    public static class DbUtils
+    {
+        public static string GetString(SqlDataReader reader, string column)
+        {
+            var ordinal = reader.GetOrdinal(column);
+            if (reader.IsDBNull(ordinal))
+            {
+                return null;
+            }
+
+            return reader.GetString(ordinal);   
+        }
+
+        public static DateTime? GetDateTime(SqlDataReader reader, string column)
+        {
+            var ordinal = reader.GetOrdinal(column);
+            if(reader.IsDBNull(ordinal))
+            {
+                return null;
+            }
+            return reader.GetDateTime(ordinal);
+        }
+
+        public static int? GetInt(SqlDataReader reader, string column)
+        {
+            var ordinal = reader.GetOrdinal(column);
+            if (reader.IsDBNull(ordinal))
+            {
+                return null;
+            }
+            return reader.GetInt32(ordinal);
+        }
+
+        public static bool? GetBool(SqlDataReader reader, string column)
+        {
+            var ordinal = reader.GetOrdinal(column);
+            if (reader.IsDBNull(ordinal))
+            {
+                return null;
+            }
+            return reader.GetBoolean(ordinal);
+        }
+
+        public static void AddParameter(SqlCommand cmd, string name, object value)
+        {
+            if(value == null)
+            {
+                cmd.Parameters.AddWithValue(name, DBNull.Value);
+            }
+            else
+            {
+                cmd.Parameters.AddWithValue(name, value);
+            }
+        }
+
+    }
+
+
+}
